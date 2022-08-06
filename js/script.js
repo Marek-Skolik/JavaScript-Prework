7f3aad15f484baeb51ca99b3f6bd5381224ef8e3
Marek Skolik <marek250296@interia.pl>
2022-08-06 16:35:13 +0200

    playerMove

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-let playerMove = ` papier `;
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+
+if(playerInput == '2'){
+    playerMove = 'papier';
+}
+
+if(playerInput == '3'){
+    playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
