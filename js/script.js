c9f69e445db5fc94bda439f614dc3cea818924ec
Marek Skolik <marek250296@interia.pl>
2022-08-08 15:32:15 +0200

    add if else

--- js/script.js
+++ js/script.js
@@ -4,15 +4,11 @@ console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = 'nieznany ruch';
 
-if(randomNumber == 1){
-  computerMove = 'kamień';
-}
-
-if(randomNumber == 2){
+if (randomNumber == 1){
+    computerMove = 'kamień';
+} else if (randomNumber == 2){
     computerMove = 'papier';
-}
-
-if(randomNumber == 3){
+}else {
     computerMove = 'nożyce';
 }
 
@@ -27,14 +23,10 @@ let playerMove = 'nieznany ruch';
 
 if(playerInput == '1'){
   playerMove = 'kamień';
-}
-
-if(playerInput == '2'){
-    playerMove = 'papier';
-}
-
-if(playerInput == '3'){
-    playerMove = 'nożyce';
+} else if (playerInput == '2'){
+  playerMove = 'papier';
+} else {
+  playerMove = 'nożyce';
 }
 
 printMessage('Mój ruch: ' + playerMove);
@@ -43,9 +35,7 @@ if(( computerMove == 'kamień' && playerMove == 'papier')||
   ( computerMove == 'nożyce' && playerMove == 'kamień')||
   ( computerMove == 'papier' && playerMove == 'nożyce')){
     printMessage ('Wygrywasz!');
-  } else if ((computerMove == 'kamień' && playerMove == 'kamień')||
-            (computerMove == 'papier' && playerMove == 'papier')||
-            (computerMove == 'nożyce' && playerMove == 'nożyce')){
+  } else if (computerMove == playerMove ){
             printMessage ('Remis!');
   } else {
     printMessage ('Przegrywasz!');
